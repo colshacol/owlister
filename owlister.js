7a3c6368cf490acc956a25b0bc5fceb6117930cb
Shane Colcleasure <colshacol@gmail.com>
2016-06-04 16:00:53 -0500

    Revamp 2

--- owlister.js
+++ owlister.js
@@ -1,86 +1,58 @@
 #!/usr/bin/env node
 'use strict'
 
-// IDEA: Store scriptsObject in global Owlister directory
-// along with copy of most previous tasks.owl file.
-// Compare files. If no diff is present, use currently
-// stored scriptsObject instead of re-parsing.
-
-// IDEA: $ owl cli-help <cli name> will give the user
-// real world examples of how to write CLI commands for
-// the specified cli tool.
+// IDEA: $ cli-help website directory.
 
 const fs = require('fs'); // file system
-// const os = process.platform; // operating system
 
 
-// read tasks.owl, return error msg or data string
+// read tasks.owl, return error message or data string.
 fs.readFile('tasks.owl', 'utf8', (err, data) => {
-  err ? console.log('err: invalid file') : true;
+  err ? console.log('err: missing tasks.owl file') : true;
 
-  data = data.replace(/\(\^\)/gim, 'npm run');
+  data = data.replace(/ => /g, ' = concurrently ');
+  data = data.replace(/\s\s+/g, ' ');
 
-  // get vars and values from var block
-  const vars = data.slice( // TODO: research slice/substr
-    data.indexOf('vars ((') + 'vars (('.length,
-    data.indexOf('))'))
-    .replace(/\n|=|;/gm, '')
-    .split(/\s/g)
-    .filter(Boolean);
+  // match all instances of "var varname = value"
+  let variables = data.match(/var [\w-:]+ = [^;]+/g);
+  // console.log(variables);
+  
+  let varsObject = {};
+  
+  variables.forEach( (variable) => {
+    variable = variable.slice(4, variable.length)
+      .split(' = ');
+      varsObject[variable[0]] = variable[1];
+  });
+  
+  let taskRequest = varsObject[process.argv[2]];
+  // console.log(taskRequest);
+  
+  let whileCount = 0;
 
-  // console.log(vars);
-  let varsLen = vars.length;
+  // while an unprocessed variable is still present in the task
+  // replace the variable with its appropriate value, and test again.
+  while(taskRequest.match(/\([\w-:]+\)/g)) {
+    
+    // grab the variable name from within the parenthesis
+    let theVar = taskRequest.slice(taskRequest.indexOf('(') + 1,
+      taskRequest.indexOf(')'));
 
-  // replace (varName) instances with var values
-  for(let i = 0; i < varsLen; i += 2) {
-    let  thisKey = `(${vars[i]})`,
-         thisVal = vars[i + 1];
-    data = data.replace(thisKey, thisVal);
-  };
+    // replace the (variable) instance with the associated value within
+    // the varsObject object.
+    taskRequest = taskRequest.replace(`(${theVar})`, varsObject[theVar]);
 
-
-  // ==================================================
-  // ==================================================
-
-  // store task blocks as variables
-  let scriptsObject = (() => {
-    let scriptsBlock = data.slice(
-      data.indexOf('scripts ((') + 'scripts (('.length,
-      data.lastIndexOf('))'))
-      .trim()
-      .replace(/[\s]{2,}|[\n]/gm, ' ')
-      .split(/;|[\s=\s]{2,}/g)
-      .filter(Boolean);
-
-    let obj = {};
-    scriptsBlock.forEach((piece, i) => {
-      if(i % 2 == 0 || i == 0) {
-        obj[piece.trim()] = scriptsBlock[i + 1];
-      };
-    })
-    return obj;
-  })();
-
-  const task = process.argv[2]; // task name
-  let thisTask = scriptsObject[task];
-
-  let whileCount = 0
-
-  while(thisTask.match(/\([\w]+\)/g)) {
-    let theTaskVar = thisTask.slice(thisTask.indexOf('(') + 1,
-      thisTask.indexOf(')'));
-
-    thisTask = thisTask.replace(`(${theTaskVar})`, scriptsObject[theTaskVar]);
-
+    // save us from any possible infinite loop.
+    whileCount++;
     if(whileCount >= 50) {
       break;
     };
   } // while
-
-  console.log(thisTask);
+  
+  // console.log(taskRequest);
 
   const exec = require('child_process').exec;
-  const child = exec(thisTask, (error, stdout, stderr) => {
+  const child = exec(taskRequest, (error, stdout, stderr) => {
   	console.log(`stdout: ${stdout}`);
   	console.log(`stderr: ${stderr}`);
   	if (error !== null) {
